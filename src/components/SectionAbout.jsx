2af3780a38f3f892707e48e9fe0261d92b2cc4ba
Júlio Elias de Sousa rocha <julioeliasdesousarocha.work@gmail.com>
2024-12-21 08:45:41 -0300

    refactor: Fixing the SectionAsbout component

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { Col, Collapse, Row } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { RiArrowDownSLine } from "react-icons/ri";
 
 const SectionAbout = () => {
   const [open, setOpen] = useState(true);
+  const [openCollapse, setOpenCollapse] = useState(false);
 
   const handleResize = () => {
     setOpen(window.innerWidth > 770);
@@ -24,22 +27,40 @@ const SectionAbout = () => {
     >
       <Card
         style={{ backgroundColor: "black" }}
-        className={`text-white m-${open ? "5" : "3"} ${
+        className={`text-white m-${open ? "4" : "3"} ${
           open ? "w-50" : "w-100"
-        } ${open ? "p-5" : "p-2"}`}
+        } ${open ? "p-4" : "p-1"}`}
       >
         <Card.Header>
-          <h2>Querendo saber mais?</h2>
+          <Row className="justify-content-center align-items-center">
+            <Col xs={open ? 10 : 9}>
+              <h2 style={{ fontSize: !open ? "19px" : undefined }}>
+                Querendo saber mais?
+              </h2>
+            </Col>
+            <Col>
+              <RiArrowDownSLine
+                onClick={() => setOpenCollapse(!openCollapse)}
+                aria-controls="example-collapse-text"
+                aria-expanded={openCollapse}
+                size={30}
+              />
+            </Col>
+          </Row>
         </Card.Header>
-        <Card.Body>
-          Esse site é perfeito para os fãs do Cabeça de Teia que querem embarcar
-          em uma maratona dos filmes do Homem-Aranha! Explore todos os filmes,
-          desde os clássicos até os mais recentes, e use nossas ferramentas para
-          ordenar e escolher o próximo da sua jornada. Com referências do
-          universo Aranha, seja você fã do Peter Parker ou do Miles Morales,
-          temos tudo para acertar na escolha do filme. Entre no &quot;Modo
-          Aranha&quot; e aproveite a maratona! 🕷️🕸️
-        </Card.Body>
+        <Collapse in={openCollapse}>
+          <div id="example-collapse-text">
+            <Card.Body>
+              Esse site é perfeito para os fãs do Cabeça de Teia que querem
+              embarcar em uma maratona dos filmes do Homem-Aranha! Explore todos
+              os filmes, desde os clássicos até os mais recentes, e use nossas
+              ferramentas para ordenar e escolher o próximo da sua jornada. Com
+              referências do universo Aranha, seja você fã do Peter Parker ou do
+              Miles Morales, temos tudo para acertar na escolha do filme. Entre
+              no &quot;Modo Aranha&quot; e aproveite a maratona! 🕷️🕸️
+            </Card.Body>
+          </div>
+        </Collapse>
       </Card>
     </section>
   );
